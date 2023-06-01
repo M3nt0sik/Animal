9df5b37e57ff10011fdb0829681d7e544ffe3b17
Kamil <mentos.96@o2.pl>
2023-06-01 22:21:03 +0200

    Write class Wolf

---
+++ Wolf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Wolf : Canine
+    {
+        public Wolf(bool belongToPack)
+        {
+            BelongsToPack = belongToPack;
+        }
+        public override void MakeNoise()
+        {
+            if (BelongsToPack)
+                Console.WriteLine("Należe do stada.");
+            Console.WriteLine("Auuuuuuuu!");
+        }
+
+        public void HuntInPack()
+        {
+            if (BelongsToPack)
+            {
+                Console.WriteLine("Wybieram się na polowanie z moim staem");
+
+            }
+            else
+            {
+                Console.WriteLine("Nie należe do stada.");
+            }
+        }
+
+    }
+}
