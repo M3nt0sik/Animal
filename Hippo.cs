068960a7cb7b3fb7e22d44a96826dc264c50fb2e
Kamil <mentos.96@o2.pl>
2023-06-01 22:10:03 +0200

    Write class Hippo

---
+++ Hippo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    internal class Hippo:Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("Chrzakanie.");
+        }
+        public void Swim()
+        {
+            Console.WriteLine("Plusk! Ie popływać!");
+        }
+
+    }
+}
